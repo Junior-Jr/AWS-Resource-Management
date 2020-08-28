2be01d29de937f2e376300799228ea7fb89d63c3
Possathon Chitpidakorn <31647547+Junior-Jr@users.noreply.github.com>
2020-08-28 17:42:59 +0700

    Can tag instance during launch

--- ec2/create_ec2.py
+++ ec2/create_ec2.py
@@ -1,11 +1,24 @@
 import boto3
-ec2 = boto3.resource('ec2')
+ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
+response = ec2_client.describe_instances()
 
 # create a new EC2 instance
-instances = ec2.create_instances(
+instances = ec2_resource.create_instances(
      ImageId='ami-0cd31be676780afa7',
      MinCount=1,
      MaxCount=1,
      InstanceType='t2.micro',
-     KeyName='ec2-keypair'
+     KeyName='ec2-keypair',
+     TagSpecifications=[
+         {
+             'ResourceType':'instance',
+             'Tags': [
+                 {
+                     'Key': 'StudentID',
+                     'Value': '60070059'
+                 }
+             ]
+         }
+     ]
  )
