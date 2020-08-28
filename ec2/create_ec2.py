9d65bd04d7433eab1e74496356572c09f229fd5e
Possathon Chitpidakorn <31647547+Junior-Jr@users.noreply.github.com>
2020-08-28 20:52:01 +0700

    Add student id input

--- ec2/create_ec2.py
+++ ec2/create_ec2.py
@@ -1,24 +1,32 @@
 import boto3
-ec2_resource = boto3.resource('ec2')
-ec2_client = boto3.client('ec2')
-response = ec2_client.describe_instances()
+ec2 = boto3.resource('ec2')
 
-# create a new EC2 instance
-instances = ec2_resource.create_instances(
-     ImageId='ami-0cd31be676780afa7',
-     MinCount=1,
-     MaxCount=1,
-     InstanceType='t2.micro',
-     KeyName='ec2-keypair',
-     TagSpecifications=[
-         {
-             'ResourceType':'instance',
-             'Tags': [
-                 {
-                     'Key': 'StudentID',
-                     'Value': '60070059'
-                 }
-             ]
-         }
-     ]
- )
+def main():
+
+    studentID = str(input("Enter Your Student ID: "))
+    create_instance(studentID)
+
+def create_instance(studentID):
+
+    instances = ec2.create_instances(
+        ImageId='ami-0cd31be676780afa7',
+        MinCount=1,
+        MaxCount=1,
+        InstanceType='t2.micro',
+        KeyName='ec2-keypair',
+        TagSpecifications=[
+            {
+                'ResourceType':'instance',
+                'Tags': [
+                    {
+                        'Key': 'StudentID',
+                        'Value': studentID
+                    }
+                ]
+            }
+        ]
+    )
+    print('EC2 Instance created')
+    print('Student ID: ' + studentID)
+
+main()
